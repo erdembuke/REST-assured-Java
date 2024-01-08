24763b65c1359157f7153c2ce682a7eb983899a9
Erdem BÜKE <erdembke@gmail.com>
2024-01-08 12:47:15 +0300

    First Test getAllBookingTests() commited

--- src/test/java/GetAllBookingsTests.java
+++ src/test/java/GetAllBookingsTests.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class GetAllBookingsTests {
+    // Create API Call
+    // https://restful-booker.herokuapp.com/booking (GET)
+    // Check the Response
+
+    @Test
+    public void getAllBookingsTest() {
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking") // URL
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
 }
