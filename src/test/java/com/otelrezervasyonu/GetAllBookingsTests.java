eb4cf39f3575b5f76209c23e05f04587eac6c455
Erdem BÜKE <erdembke@gmail.com>
2024-01-09 20:52:04 +0300

    Query Parametreleri kullanilarak getallBookingsByQueryParam testi yazildi

--- src/test/java/com/otelrezervasyonu/GetAllBookingsTests.java
+++ src/test/java/com/otelrezervasyonu/GetAllBookingsTests.java
@@ -1,6 +1,10 @@
 package com.otelrezervasyonu;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -16,6 +20,29 @@ public class GetAllBookingsTests extends BaseTest{
                 .get("/booking") // URL
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void getAllBookingsByQueryParam() {
+        // creating new reservation
+        int id = createBookingId();
+        // add query param
+        spec.queryParam("firstname","Erdem");
+        spec.queryParam("lastname","Buke");
+        // make api call
+        Response response = given(spec)
+                .when()
+                .get("/booking");
+
+        // assertion
+        response.then()
+                .statusCode(200);
+
+        // response'da bulunan butun id leri liste olusturup icine ekledik
+        List<Integer> filteredReservations = response.jsonPath().getList("bookingid");
+        // listedeki elemanlari kontrol edip istedigimiz reservation id yi bul
+        Assertions.assertTrue(filteredReservations.contains(id));
+
     }
 
 }
